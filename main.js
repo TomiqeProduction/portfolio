0777668e5ef1e5d9702fc08662ac55ee86e8d0bb
Tomiqe Production <67323286+TomiqeProduction@users.noreply.github.com>
2021-11-23 17:43:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', function() {
-    this.classList.toggle('is-active');
+    this.classList.toggle('sidepanel--active');
 });
