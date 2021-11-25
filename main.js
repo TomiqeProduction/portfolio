59f4675b7c11897c84d3410ee7d893aa71528827
Tomiqe Production <67323286+TomiqeProduction@users.noreply.github.com>
2021-11-25 01:39:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector (".menu");
+const sidepanel = document.querySelector (".sidepanel");
+const sidepanel__link = document.querySelectorAll("sidepanel__link")
 
-hamburger.addEventListener('click', function() {
-    this.classList.toggle('sidepanel--active');
+// main toggle
+menu.addEventListener("click",() => {
+    toggle();
+});
+
+// toggle on item click if open
+sidepanel__link.forEach((item) => {
+    item.addEventListener("click", () => {
+        if(menu.classList.contains("open")) {
+            toggle();
+        }
+    })
+})
+
+function toggle() {
+    menu.classList.toggle("open");
+    sidepanel.classList.toggle("open");
+}
+
+$(document).ready(function() {
+    $('.sidepanel__link').on('click', function() {
+        if ($('.sidepanel').hasClass('open')){
+            $('.menu').removeClass('open');
+            $('.sidepanel').removeClass('open');
+        }
+    });
 });
